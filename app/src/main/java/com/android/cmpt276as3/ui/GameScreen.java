77bb36964ba7ff8ba11231c4d3168bfdbbc6a128
phyeony <phyeony@gmail.com>
2020-10-13 20:37:17 -0700

    Add and scaled pokemon img when button is clicked

--- app/src/main/java/com/android/cmpt276as3/ui/GameScreen.java
+++ app/src/main/java/com/android/cmpt276as3/ui/GameScreen.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +20,10 @@ import com.android.cmpt276as3.R;
 
 public class GameScreen extends AppCompatActivity {
     private static final String TAG = "GameScreen";
-    private static final int NUM_ROWS = 3;
-    private static final int NUM_COLS = 5;
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 10;
+
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
 
     public static Intent makeLaunchIntent(Context context) {
         return new Intent(context, GameScreen.class);
@@ -43,6 +49,9 @@ public class GameScreen extends AppCompatActivity {
             table.addView(tableRow);
 
             for(int col = 0 ; col < NUM_COLS; col++){
+                final int FINAL_ROW = row;
+                final int FINAL_COL = col;
+
                 Button btn = new Button(this);
                 btn.setBackgroundResource(R.drawable.tall_grass);
                 btn.setLayoutParams(new TableRow.LayoutParams(
@@ -53,17 +62,47 @@ public class GameScreen extends AppCompatActivity {
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        gridButtonClicked();
+                        gridButtonClicked(FINAL_ROW, FINAL_COL);
                     }
                 });
 
                 tableRow.addView(btn);
+                buttons[row][col] = btn;
             }
         }
     }
 
-    private void gridButtonClicked() {
+    private void gridButtonClicked(int row, int col) {
         Toast.makeText(this, "btn clicked",Toast.LENGTH_SHORT).show();
+        Button btn = buttons[row][col];
+
+        //Lock Button Sizes:
+        lockButtonSizes();
+
+        //Scale image to button
+        int newWidth = btn.getWidth();
+        int newHeight = btn.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.charmander);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        btn.setBackground(new BitmapDrawable(resource, scaledBitmap));
+
+    }
+
+    private void lockButtonSizes() {
+        for(int row = 0; row < NUM_ROWS; row++){
+            for(int col = 0; col < NUM_COLS; col++){
+                Button btn = buttons[row][col];
+
+                int width = btn.getWidth();
+                btn.setMinWidth(width);
+                btn.setMaxWidth(width);
+
+                int height = btn.getHeight();
+                btn.setMinHeight(height);
+                btn.setMaxHeight(height);
+            }
+        }
     }
 
 }
