7e8371c786e5fcc5d0757dd722680311abd8e1d9
phyeony <phyeony@gmail.com>
2020-10-13 19:46:34 -0700

    Make tables for button

--- app/src/main/java/com/android/cmpt276as3/ui/GameScreen.java
+++ app/src/main/java/com/android/cmpt276as3/ui/GameScreen.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 import com.android.cmpt276as3.R;
 
 public class GameScreen extends AppCompatActivity {
     private static final String TAG = "GameScreen";
+    private static final int NUM_ROWS = 3;
+    private static final int NUM_COLS = 5;
 
     public static Intent makeLaunchIntent(Context context) {
         return new Intent(context, GameScreen.class);
@@ -20,5 +28,42 @@ public class GameScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
         this.setTitle(TAG);
+
+        populateButtons();
     }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+        for(int row = 0; row < NUM_ROWS; row ++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+            table.addView(tableRow);
+
+            for(int col = 0 ; col < NUM_COLS; col++){
+                Button btn = new Button(this);
+                btn.setBackgroundResource(R.drawable.tall_grass);
+                btn.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        gridButtonClicked();
+                    }
+                });
+
+                tableRow.addView(btn);
+            }
+        }
+    }
+
+    private void gridButtonClicked() {
+        Toast.makeText(this, "btn clicked",Toast.LENGTH_SHORT).show();
+    }
+
 }
