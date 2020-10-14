dfd1303e313fe4a81d33356613e70dfaaa018d1e
phyeony <phyeony@gmail.com>
2020-10-13 17:03:08 -0700

    3>git commit -m

--- app/src/main/java/com/android/cmpt276as3/ui/MainMenu.java
+++ app/src/main/java/com/android/cmpt276as3/ui/MainMenu.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.cmpt276as3.R;
 
@@ -20,5 +22,48 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         this.setTitle(TAG);
+        setUpStartGameButton();
+        setUPOptionsButton();
+        setUpHelpButton();
     }
+
+    private void setUpStartGameButton() {
+        Button btn= findViewById(R.id.btnStartGame);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = GameScreen.makeLaunchIntent(MainMenu.this);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void setUPOptionsButton() {
+        Button btn= findViewById(R.id.btnOptions);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = OptionsScreen.makeLaunchIntent(MainMenu.this);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void setUpHelpButton() {
+        Button btn= findViewById(R.id.btnHelp);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = HelpScreen.makeLaunchIntent(MainMenu.this);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+
 }
