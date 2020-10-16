d1e076ecf83c485e33075237f95b142bcda14d6e
ACimensel <cimensel.a@hotmail.com>
2020-10-16 01:46:17 -0700

    [acimense] Made aesthetic changes to the activities

--- app/src/main/java/com/android/cmpt276as3/ui/WelcomeScreen.java
+++ app/src/main/java/com/android/cmpt276as3/ui/WelcomeScreen.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import com.android.cmpt276as3.R;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class WelcomeScreen extends AppCompatActivity {
     private static final String TAG = "WelcomeScreen";
+    private GifImageView gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,29 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
         this.setTitle(TAG);
 
+        gif = findViewById(R.id.loading_pokeball);
+
         Button butMain = findViewById(R.id.button_gotomain);
         butMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = MainMenu.makeLaunchIntent(WelcomeScreen.this);
-                startActivity(i);
+                gif.setAlpha(1f);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = MainMenu.makeLaunchIntent(WelcomeScreen.this);
+                        startActivity(i);
+                    }
+                },2500);
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        gif.setAlpha(0f);
     }
 }
